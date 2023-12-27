a29f7ed2de71924e4dc28c475dc3a8f092523634
derickteo <96604131+derickteo@users.noreply.github.com>
2023-12-27 15:35:26 +0800

    Update app_chat.py

--- app_chat.py
+++ app_chat.py
@@ -5,7 +5,7 @@ import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv()
-#GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
 new_chat_id = f'{time.time()}'
